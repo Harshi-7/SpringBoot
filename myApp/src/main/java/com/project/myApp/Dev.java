2deb05c99d75a56e2a42aa188a8970e04d717a1a
Harshi-7 <harshithabharadi7104@gmail.com>
2025-12-03 18:01:12 +0530

    SpringBoot Application

---
+++ myApp/src/main/java/com/project/myApp/Dev.java
@@ -0,0 +1,12 @@
+package com.project.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("hi harshuu");
+    }
+
+}
